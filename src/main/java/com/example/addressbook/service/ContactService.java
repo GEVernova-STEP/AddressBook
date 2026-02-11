91a6d7c43cf025541ff10fa3f4b77fdd68b97886
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2026-02-12 00:50:13 +0530

    [Ashutosh] feat: enabled logging

--- src/main/java/com/example/addressbook/service/ContactService.java
+++ src/main/java/com/example/addressbook/service/ContactService.java
@@ -4,6 +4,7 @@ import com.example.addressbook.dto.ContactDTO;
 import com.example.addressbook.exception.ResourceNotFoundException;
 import com.example.addressbook.model.Contact;
 import com.example.addressbook.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 
 // Service layer manages business logic
 @Service
+@Slf4j
 public class ContactService {
 
     @Autowired
